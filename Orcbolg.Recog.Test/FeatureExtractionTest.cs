7713777e55e4b0f896a0e8221782ed6cb0ef8208
sinshu <sinshu.d@gmail.com>
2019-04-15 18:26:14 +0900

    test

--- Orcbolg.Recog.Test/FeatureExtractionTest.cs
+++ Orcbolg.Recog.Test/FeatureExtractionTest.cs
@@ -13,7 +13,7 @@ namespace Orcbolg.Recog.Test
     public class FeatureExtractionTest
     {
         [TestMethod]
-        public void GetNormalizedAmplitude()
+        public void GetNormalizedAmplitude1()
         {
             var frameLength = 16;
             var buffer = new Complex[frameLength];
@@ -37,6 +37,79 @@ namespace Orcbolg.Recog.Test
                         Assert.AreEqual(0.0, amplitude[i], 0.001);
                     }
                 }
+            }
+        }
+
+        [TestMethod]
+        public void GetNormalizedAmplitude2()
+        {
+            var scale = 3.1;
+            var frameLength = 16;
+            var buffer = new Complex[frameLength];
+            var amplitude = new double[frameLength / 2];
+            for (var w = 0; w < frameLength / 2; w++)
+            {
+                for (var t = 0; t < frameLength; t++)
+                {
+                    buffer[t] = Math.Cos(2 * Math.PI * w * t / frameLength);
+                }
+                Fourier.Forward(buffer, FourierOptions.AsymmetricScaling);
+                FeatureExtraction.GetNormalizedAmplitude(buffer, amplitude, scale);
+                for (var i = 0; i < amplitude.Length; i++)
+                {
+                    if (i == w)
+                    {
+                        Assert.AreEqual(scale, amplitude[i], 0.001);
+                    }
+                    else
+                    {
+                        Assert.AreEqual(0.0, amplitude[i], 0.001);
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void GetAmplitudeWithTriangularFilter1()
+        {
+            var value = 3.1;
+            var length = 1024;
+            var source = EnumerableEx.Repeat(value, length).ToArray();
+            var random = new Random(2357);
+            for (var i = 0; i < 100; i++)
+            {
+                var indices = Enumerable.Range(0, 3).Select(x => length * random.NextDouble()).ToArray();
+                Array.Sort(indices);
+                var actual = FeatureExtraction.GetAmplitudeWithTriangularFilter(source, indices[0], indices[1], indices[2]);
+                Assert.AreEqual(value, actual, 0.001);
+            }
+        }
+
+        [TestMethod]
+        public void GetAmplitudeWithTriangularFilter2()
+        {
+            var value = 3.1;
+            var length = 1024;
+            var source = EnumerableEx.Repeat(value, length).ToArray();
+            var random = new Random(2357);
+            for (var i = 0; i < length - 1; i++)
+            {
+                var actual = FeatureExtraction.GetAmplitudeWithTriangularFilter(source, i, i + 1, i + 2);
+                Assert.AreEqual(value, actual, 0.001);
+            }
+        }
+
+        [TestMethod]
+        public void GetAmplitudeWithTriangularFilter3()
+        {
+            var value = 3.1;
+            var length = 1024;
+            var source = EnumerableEx.Repeat(value, length).ToArray();
+            var random = new Random(2357);
+            for (var i = 0; i < length; i++)
+            {
+                var actual = FeatureExtraction.GetAmplitudeWithTriangularFilter(source, i, i, i);
+                Assert.AreEqual(value, actual, 0.001);
             }
         }
     }
--- Orcbolg.Recog/FeatureExtraction.cs
+++ Orcbolg.Recog/FeatureExtraction.cs
@@ -8,6 +8,8 @@ namespace Orcbolg.Recog
 {
     public static class FeatureExtraction
     {
+        private static readonly double epsilon = 10E-9;
+
         public static void GetNormalizedAmplitude(Complex[] source, double[] destination)
         {
             if (source == null) throw new ArgumentNullException(nameof(source));
@@ -20,6 +22,71 @@ namespace Orcbolg.Recog
             for (var w = 1; w < destination.Length; w++)
             {
                 destination[w] = source[w].Magnitude / half;
+            }
+        }
+
+        public static void GetNormalizedAmplitude(Complex[] source, double[] destination, double scale)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (destination == null) throw new ArgumentNullException(nameof(destination));
+
+            var half = source.Length / 2;
+            if (destination.Length != half) throw new ArgumentException("Length of destination must be half the length of source.");
+
+            destination[0] = scale * source[0].Magnitude / source.Length;
+            for (var w = 1; w < destination.Length; w++)
+            {
+                destination[w] = scale * source[w].Magnitude / half;
+            }
+        }
+
+        public static double GetAmplitudeWithTriangularFilter(double[] source, double lowerIndex, double centerIndex, double upperIndex)
+        {
+            var num = 0.0;
+            var den = 0.0;
+            var count = 0;
+
+            var leftWidth = centerIndex - lowerIndex;
+            if (leftWidth >= epsilon)
+            {
+                var start = (int)Math.Floor(lowerIndex);
+                var end = (int)Math.Ceiling(centerIndex) - 1;
+                for (var w = start; w <= end; w++)
+                {
+                    var binLeft = w == start ? lowerIndex : w;
+                    var binRight = w == end ? centerIndex : w + 1;
+                    var binCenter = (binLeft + binRight) / 2;
+                    var binArea = (binCenter - lowerIndex) / leftWidth * (binRight - binLeft);
+                    num += binArea * source[w];
+                    den += binArea;
+                    count++;
+                }
+            }
+
+            var rightWidth = upperIndex - centerIndex;
+            if (rightWidth >= epsilon)
+            {
+                var start = (int)Math.Floor(centerIndex);
+                var end = (int)Math.Ceiling(upperIndex) - 1;
+                for (var w = start; w <= end; w++)
+                {
+                    var binLeft = w == start ? centerIndex : w;
+                    var binRight = w == end ? upperIndex : w + 1;
+                    var binCenter = (binLeft + binRight) / 2;
+                    var binArea = (1 - (binCenter - centerIndex) / rightWidth) * (binRight - binLeft);
+                    num += binArea * source[w];
+                    den += binArea;
+                    count++;
+                }
+            }
+
+            if (count > 0)
+            {
+                return num / den;
+            }
+            else
+            {
+                return source[(int)Math.Floor(centerIndex)];
             }
         }
     }
